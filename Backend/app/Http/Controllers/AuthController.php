7d1d01a5cb9e443d8a3df399eb01e409352c2b35
Dimas-WA <109815005+Dimas-WA@users.noreply.github.com>
2022-07-26 23:17:37 +0700

    Login

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\UserProfile;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Hash;
+use Auth;
 use Validator;
 class AuthController extends Controller
 {public function register(Request $request)
@@ -44,5 +45,20 @@ class AuthController extends Controller
         return response()
             ->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer', ]);
 
+    }
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('username', 'password')))
+        {
+            return response()
+                ->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('username', $request['username'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['message' => 'Hi '.$user->name.', welcome to home','access_token' => $token, 'token_type' => 'Bearer', ]);
     }
 }
