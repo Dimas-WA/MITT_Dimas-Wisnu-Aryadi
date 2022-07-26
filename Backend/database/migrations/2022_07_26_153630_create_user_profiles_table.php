984aba7bd07fde1648c079e9f27680b825d51a02
Dimas-WA <109815005+Dimas-WA@users.noreply.github.com>
2022-07-26 22:45:55 +0700

    Migration Table User Profile

---
+++ Backend/database/migrations/2022_07_26_153630_create_user_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->string('username');
+            $table->string('name');
+            $table->text('address');
+            $table->date('bod');
+            $table->string('email')->unique();
+            $table->timestamps();
+            $table->foreign('username')
+            ->references('username')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
